d1c375df97d81f084292cc5ef6880828906510e2
Oskar Videll-Jane <ovidelljane@gmail.com>
2024-12-06 11:41:58 +1100

    Boat has code
    boat has code above them

--- LighthouseGameProject/Assets/Scripts/BoatMovement.cs
+++ LighthouseGameProject/Assets/Scripts/BoatMovement.cs
@@ -1,4 +1,7 @@
+using Mono.Cecil.Cil;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoatMovement : MonoBehaviour
 {
@@ -6,13 +9,23 @@ public class BoatMovement : MonoBehaviour
     public float speed = 2.0f;
     public float turnSpeed = 50.0f;
     
+    public TextMeshPro textAboveBoat;
+
+    private string boatCode;
+    
     private bool isTurning = false;
     private Quaternion targetRotation;
     private GridManager gridManager;
+    
 
     private void Start()
     {
         gridManager = FindObjectOfType<GridManager>();
+        boatCode = codeGenerator();
+        if (textAboveBoat != null)
+        {
+            textAboveBoat.text = boatCode;
+        }
     }
 
     void Update()
@@ -56,6 +69,18 @@ public class BoatMovement : MonoBehaviour
         {
             Destroy(gameObject); // Despawn the boat if it's outside the grid
         }
+    }
+
+    private string codeGenerator()
+    {
+        string st = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        char c = st[Random.Range(0, st.Length)];
+        int num = Random.Range(10, 99);
+
+        string code = c + num.ToString();
+        return code;
+
     }
 
 }
